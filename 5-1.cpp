576a91e18ca6278f1b90031f34dd15709a83436a
Artyom Kuzkov <mc.ky3r@gmail.com>
2020-04-15 19:51:09 +0300

    5-1.cpp update

--- 5-1.cpp
+++ 5-1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <limits>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -22,17 +24,25 @@ private:
 public:
     Vector3D();
     Vector3D(double, double, double);
+
     ~Vector3D();
+
+    inline double getX();
+    inline double getY();
+    inline double getZ();
 
     inline double length();
     inline const Vector3D& normalize();
+    inline Vector3D getNormalized();
+    inline Vector3D project(const Vector3D&);
 
     inline Vector3D& operator+=(const Vector3D&);
     inline Vector3D& operator-=(const Vector3D&);
     inline Vector3D& operator*=(const double);
+    inline Vector3D& operator/=(const double);
 
-    inline const Vector3D operator-();
-    inline const Vector3D operator+();
+    inline Vector3D operator-();
+    inline Vector3D operator+();
 
     friend ostream& operator<<(ostream&, const Vector3D&);
     friend istream& operator>>(istream&, Vector3D&);
@@ -40,29 +50,53 @@ public:
     friend inline bool operator==(const Vector3D&, const Vector3D&);
     friend inline bool operator!=(const Vector3D&, const Vector3D&);
 
-    friend inline const Vector3D operator+(const Vector3D&, const Vector3D&);
-    friend inline const Vector3D operator-(const Vector3D&, const Vector3D&);
+    friend inline Vector3D operator+(const Vector3D&, const Vector3D&);
+    friend inline Vector3D operator-(const Vector3D&, const Vector3D&);
 
     friend inline double operator*(const Vector3D&, const Vector3D&);
-    friend inline const Vector3D operator*(const double, const Vector3D&);
-    friend inline const Vector3D operator*(const Vector3D&, const double);
+    friend inline Vector3D operator*(const double, const Vector3D&);
+    friend inline Vector3D operator*(const Vector3D&, const double);
+    friend inline Vector3D operator/(const Vector3D&, const double);
 
-    friend inline const Vector3D vecMult(const Vector3D&, const Vector3D&);
+    friend inline Vector3D vecMult(const Vector3D&, const Vector3D&);
 };
+
+Vector3D getResultVector(Vector3D*, Vector3D*);
+void setRandomVectors(Vector3D*, Vector3D*);
 
 int main()
 {
-    Vector3D v1(1, 0, 0);
-    Vector3D v2(1, -1, 1);
-    Vector3D* v3 = new Vector3D(1, 3 ,4);
-    Vector3D v4;
+    srand(time(NULL));  
 
-    v1 += +v2;
-    v1 = -v1 + *v3;
+    const int N = 2;
 
-    delete v3;
+    Vector3D* vecArr = new Vector3D[N];
+    setRandomVectors(vecArr, vecArr + N);
+
+    Vector3D result = getResultVector(vecArr, vecArr + N);
+    cout << result << endl;
+
+    delete[] vecArr;
 
     return 0;
+}
+
+
+
+Vector3D getResultVector(Vector3D *beg, Vector3D *end)
+{
+    Vector3D result;
+
+    for (; beg < end; beg++)
+        result += *beg;
+    
+    return result;
+}
+
+void setRandomVectors(Vector3D *beg, Vector3D *end)
+{
+    for (; beg < end; beg++)
+        *beg = Vector3D(rand() % 100, rand() % 100, rand() % 100);
 }
 
 
@@ -77,9 +111,28 @@ Vector3D::Vector3D(double x_, double y_, double z_) : x(x_), y(y_), z(z_)
     cout << "[*] Vector3D(double x_, double y_, double z_)" << endl;
 }
 
+
+
 Vector3D::~Vector3D()
 {
     cout << "[*] ~Vector3D()" << endl;
+}
+
+
+
+inline double Vector3D::getX()
+{
+    return x;
+}
+
+inline double Vector3D::getY()
+{
+    return y;
+}
+
+inline double Vector3D::getZ()
+{
+    return z;
 }
 
 
@@ -91,15 +144,31 @@ inline double Vector3D::length()
 
 inline const Vector3D& Vector3D::normalize()
 {
+    return *this = this->getNormalized();
+}
+
+inline Vector3D Vector3D::getNormalized()
+{
     double len = this->length();
 
     if (isEqual(len, 0))
         throw logic_error("Normalization error: null vector can't be normalized");
 
-    x = x / len;
-    y = y / len;
-    z = z / len;
-    return *this;
+    return Vector3D(x / len, y / len, z / len);
+}
+
+inline Vector3D Vector3D::project(const Vector3D &vec)
+{
+    try
+    {
+        Vector3D normal = this->getNormalized();
+
+        return normal * (vec * normal);
+    }
+    catch (logic_error err)
+    {
+        throw logic_error("Projection error: vector can't be projected onto null vector");
+    }
 }
 
 
@@ -119,14 +188,19 @@ inline Vector3D& Vector3D::operator*=(const double a)
     return *this = a * *this;
 }
 
+inline Vector3D& Vector3D::operator/=(const double a)
+{
+    return *this = *this / a;
+}
 
 
-inline const Vector3D Vector3D::operator-()
+
+inline Vector3D Vector3D::operator-()
 {
     return Vector3D(-this->x, -this->y, -this->z);
 }
 
-inline const Vector3D Vector3D::operator+()
+inline Vector3D Vector3D::operator+()
 {
     return *this;
 }
@@ -159,12 +233,12 @@ inline bool operator!=(const Vector3D &v1, const Vector3D &v2)
 
 
 
-inline const Vector3D operator+(const Vector3D &v1, const Vector3D &v2)
+inline Vector3D operator+(const Vector3D &v1, const Vector3D &v2)
 {
     return Vector3D(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
 }
 
-inline const Vector3D operator-(const Vector3D &v1, const Vector3D &v2)
+inline Vector3D operator-(const Vector3D &v1, const Vector3D &v2)
 {
     return Vector3D(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
 }
@@ -176,19 +250,24 @@ inline double operator*(const Vector3D &v1, const Vector3D &v2)
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
-inline const Vector3D operator*(const double a, const Vector3D &vec)
+inline Vector3D operator*(const double a, const Vector3D &vec)
 {
     return Vector3D(vec.x * a, vec.y * a, vec.z * a);
 }
 
-inline const Vector3D operator*(const Vector3D &vec, const double a)
+inline Vector3D operator*(const Vector3D &vec, const double a)
 {
     return a * vec;
 }
 
+inline Vector3D operator/(const Vector3D &vec, const double a)
+{
+    return Vector3D(vec.x / a, vec.y / a, vec.z / a);
+}
 
 
-inline const Vector3D vecMult(const Vector3D &v1, const Vector3D &v2)
+
+inline Vector3D vecMult(const Vector3D &v1, const Vector3D &v2)
 {
     return Vector3D(v1.y * v2.z - v1.z * v2.y,
                     v1.z * v2.x - v1.x * v2.z,
